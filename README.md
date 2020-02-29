fce52384a61a91174bf0c01eca8d8aca6dfd4aae
Cyrus Vorazan <cyrusvorazan@gmail.com>
2020-02-29 10:09:54 +0200

    Remove unused code

--- README.md
+++ README.md
@@ -7,6 +7,7 @@ This project uses CMake to generate solution files for different versions of Vis
 Install CMake first. If you have it in your PATH variable you can generate and build the project as follows:
 
 Generate: cmake -A Win32 ..
+
 Build: cmake --build .
 
 Note that this project has to be Win32 in order for it to work.
--- inject/callback.cpp
+++ inject/callback.cpp
@@ -1,7 +1,6 @@
 #include "callback.h"
 
 #include "th09address.h"
-#include "state.h"
 
 #include <Windows.h>
 #include <iostream>
@@ -11,28 +10,24 @@ namespace th09mp
    // Global state
    th09mp::address::Th9GlobalVer1_5* g = address::globals_ver1_5;
 
-   // Saved state
-   th09mp::address::Th9GlobalVer1_5* g_saved;
-
    // Utilities
    void PrintState()
    {
       SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), { 0, 0 });
 
-      std::cout << (int)g->rng[0] << std::endl;
-      std::cout << (int)g->rng[1] << std::endl;
+      // Print stuff here
    }
 
    void HandleInput()
    {
       if ((g->key_states[PlayerSide::Side_1P].keys & Keys::d) && !(g->key_states[PlayerSide::Side_1P].prev_keys & Keys::d))
       {
-         CopyState(g, g_saved);
+         
       }
 
       if ((g->key_states[PlayerSide::Side_1P].keys & Keys::r) && !(g->key_states[PlayerSide::Side_1P].prev_keys & Keys::r))
       {
-         CopyState(g_saved, g);
+         
       }
    }
 
@@ -43,23 +38,16 @@ namespace th09mp
       {
          PrintState();
          HandleInput();
-         //g->rng[0] = 0; //rand();
-         //g->rng[1] = 0; //rand();
       }
 
       void OnGameStart(void)
       {
-         //srand(1);
-         // Initialize save state
-         g_saved = new address::Th9GlobalVer1_5();
+
       }
 
       void OnGameEnd(void)
       {
-         // Free save state
-         // TODO: clean it up properly
-         if (g_saved)
-            delete g_saved;
+
       }
    }
 }
