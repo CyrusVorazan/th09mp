d276ee611f7df7e35fa881c6d041c32d84464d70
32th System <intensonat@gmail.com>
2020-03-01 21:58:42 +0100

    Breakpoints: explain how to use them

--- inject/breakpoint.h
+++ inject/breakpoint.h
@@ -22,6 +22,19 @@ namespace th09mp {
 		False = false,
 		True = true
 	};
+	
+	/*
+	* Writing a breakpoint function:
+
+	* The Registers parameter is a structure that describes all registers. When the function returns and you didn't write to this structure, all the registers will be the same when you get back to game code
+	* If however you changed a member in the structure, then the register described by the member you wrote to will be different. The value of the register will be the value you wrote to the structure
+
+	* The return value will determine if a copy of the code that got overwritten for the breakpoint call should be executed. The place in which that copy is will always jmp to the instruction after the breakpoint call
+	  and NOP instructions if the cavesize is larger than 5
+
+	* If you want a breakpoint to skip a bunch of code, you can write to the Return member. You can basically treat this like eip. But then make sure that you return BPCaveExec::False
+	* If you change esp, the new value MUST be higher than the original esp. The purpouse of this is to be able to cleanup the stack if you put the breakpoint call on a function call
+	*/
 
 	typedef th09mp::BPCaveExec (*BPFunc_t)(struct x86Regs* Registers, void* BPParams);
 
@@ -52,6 +65,22 @@ namespace th09mp {
 		struct BPInfo BPInfo;
 		char ParamsAndCodecave[];
 	};
+	
+	/*
+	* Create a thcrap style breakpoint
+	* What this will do is create a call to code from this DLL, then return to gamecode with all registers exactly as they were, unless you changed them
 
+	* addr: address in th09 code that will trigger the breakpoint
+	* Func: Function that should be executed when the breakpoint hits.
+	* BPParams: a structure that describes the parameters of the breakpoint. 
+		Recommended ussage: declaring a structure type to hold information, passing a pointer to it to BPCreate (make sure it's allocated on the heap) 
+		and in the breakpoint function, declare a pointer to the structure type and set the address it points to to the same one that BPParams points to
+	* BPParamsSize: The size of the parameter strucutre
+		If you did the recommended thing with BPParams, this parameter should be sizeof(BPParamsStructure)
+	* Cavesize: This number needs to be at least 5; the amount of code bytes that should be overwritten at the place the breakpoint call should be placed
+		Example: you want to put a breakpoint on a  "mov dword ptr ds:[esi], 0x49A964" instruction which is 6 bytes. The cavesize should be 6
+		Example 2: You want to put a breakpoint on a "xor eax,eax" instruction. This instruction is 2 bytes. The next instruction is "and dword ptr ds:[ecx+0x80], eax" which is 6 bytes. The cavesize should be 8
+	*/
+	
 	void BPCreate(uint32_t addr, th09mp::BPFunc_t Func, void* BPParams, int BPParamsSize, int Cavesize);
 }
