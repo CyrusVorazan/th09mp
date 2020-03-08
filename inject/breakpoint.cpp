4aa2f9241d96c495b29e3116017b17b164e8fd28
32th System <intensonat@gmail.com>
2020-03-08 17:37:33 +0100

    Fix breakpoints

--- inject/breakpoint.cpp
+++ inject/breakpoint.cpp
@@ -15,7 +15,7 @@ namespace th09mp {
 			Regs->Return = BPInfo->Cave;
 		}
 
-		uint32_t esp_difference;
+		uint32_t esp_difference = 0;
 		if (esp_previous != Regs->esp) {
 			esp_difference = Regs->esp - esp_previous;
 			memmove((char*)Regs + esp_difference, Regs, sizeof(th09mp::x86Regs));
@@ -31,11 +31,15 @@ namespace th09mp {
 		}
 		struct th09mp::BP* Breakpoint = static_cast<th09mp::BP*>(::VirtualAlloc(NULL, sizeof(th09mp::BP) + BPParamsSize + Cavesize + 9, MEM_COMMIT | MEM_RESERVE, PAGE_EXECUTE_READWRITE));
 
-		memcpy(&Breakpoint->ParamsAndCodecave[0], BPParams, BPParamsSize);
+		if (BPParams != nullptr) {
+			memcpy(&Breakpoint->ParamsAndCodecave[0], BPParams, BPParamsSize);
+		}
+
 		Breakpoint->BPInfo.BPParams = &Breakpoint->ParamsAndCodecave[0];
 		
 		
 		memcpy(&Breakpoint->EntryCode[0], th09mp::BPEntry, sizeof(th09mp::BPEntry));
+		*(void**)&Breakpoint->EntryCode[4] = &Breakpoint->BPInfo;
 		memset(&Breakpoint->int3Padding[0], 0xCC, 3);
 
 		SetJumpTo(&Breakpoint->EntryCode[9], (int)&Breakpoint->EntryCode[13], (int)BPProcess);
@@ -48,13 +52,11 @@ namespace th09mp {
 		Breakpoint->BPInfo.Cave[Cavesize] = 0xE9;
 		SetJumpTo(&Breakpoint->BPInfo.Cave[Cavesize + 1], (int)&Breakpoint->BPInfo.Cave[Cavesize + 5], addr + Cavesize);
 
-		#define BPCave Breakpoint->BPInfo.Cave
-
 		int BPCaveByte = 0;
 		while(BPCaveByte < Cavesize) {
-			if (BPCave[BPCaveByte] == 0xE9 || BPCave[BPCaveByte] == 0xE8) {
-				uint32_t CallAddr = addr + *(int*)&BPCave[BPCaveByte + 1] + 5;
-				SetJumpTo(&BPCave[BPCaveByte + 1], (int)&BPCave[BPCaveByte + 5], CallAddr);
+			if ((Breakpoint->BPInfo.Cave[BPCaveByte] & 0xE8) || (Breakpoint->BPInfo.Cave[BPCaveByte] & 0xE9)) {
+				uint32_t CallAddr = addr + *(int*)&Breakpoint->BPInfo.Cave[BPCaveByte + 1] + 5;
+				SetJumpTo(&Breakpoint->BPInfo.Cave[BPCaveByte + 1], (int)&Breakpoint->BPInfo.Cave[BPCaveByte + 5], CallAddr);
 				BPCaveByte += 5;
 				continue;
 			}
